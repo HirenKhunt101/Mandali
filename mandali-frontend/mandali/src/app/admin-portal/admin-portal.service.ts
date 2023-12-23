9e5c545ca35a8293c58d8c61d93e35424d54a951
utsav <utsavjogani610@gmail.com>
2023-12-23 23:53:54 +0530

    local setup.

--- mandali-frontend/mandali/src/app/admin-portal/admin-portal.service.ts
+++ mandali-frontend/mandali/src/app/admin-portal/admin-portal.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
+import { environment } from '../../enviroments/environment';
 
 @Injectable({
   providedIn: 'root',
