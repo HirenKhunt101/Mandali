65277b4dcc27ab138f0325f400dd286d89f186c5
Hiren Khunt <hirenkhunt5960@gmail.com>
2024-01-06 12:07:39 +0530

    Implement loader

--- mandali-frontend/mandali/src/app/app.component.ts
+++ mandali-frontend/mandali/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgxUiLoaderModule, NgxUiLoaderRouterModule } from "ngx-ui-loader";
+import { HttpClientModule } from "@angular/common/http";
+
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { HeaderComponent } from './header/header.component';
 import { LoaderComponent } from './loader/loader.component';
@@ -10,7 +12,7 @@ import { LoaderComponent } from './loader/loader.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, PageNotFoundComponent, HeaderComponent, LoaderComponent],
+  imports: [CommonModule, RouterOutlet, PageNotFoundComponent, HeaderComponent, LoaderComponent, HttpClientModule, NgxUiLoaderModule, NgxUiLoaderRouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- mandali-frontend/mandali/src/app/app.config.ts
+++ mandali-frontend/mandali/src/app/app.config.ts
@@ -5,12 +5,53 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideFirebaseApp, getApp, initializeApp } from '@angular/fire/app';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 
+import {
+  NgxUiLoaderModule,
+  NgxUiLoaderConfig,
+  SPINNER,
+  POSITION,
+  PB_DIRECTION,
+} from "ngx-ui-loader";
+
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+
+
+const ngxUiLoaderConfig: any = {
+  "bgsColor": "red",
+  "bgsOpacity": 1,
+  "bgsPosition": "center-center",
+  "bgsSize": 80,
+  "bgsType": "three-bounce",
+  "blur": 15,
+  "delay": 0,
+  "fastFadeOut": true,
+  "fgsColor": "#04aa6d",
+  "fgsPosition": "center-center",
+  "fgsSize": 90,
+  "fgsType": "three-strings",
+  "gap": 50,
+  "logoPosition": "center-center",
+  "logoSize": 70,
+  "logoUrl": "",
+  "masterLoaderId": "master",
+  "overlayBorderRadius": "0",
+  "overlayColor": "rgba(40, 40, 40, 0.8)",
+  "pbColor": "#04aa6d",
+  "pbDirection": "ltr",
+  "pbThickness": 3,
+  "hasProgressBar": true,
+  "text": "Please Wait...",
+  "textColor": "#000000",
+  "textPosition": "center-center",
+  "maxTime": -1,
+  "minTime": 300
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideHttpClient(), importProvidersFrom([
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideStorage(() => getStorage()),
+    NgxUiLoaderModule.forRoot(ngxUiLoaderConfig)
   ])]
 };
